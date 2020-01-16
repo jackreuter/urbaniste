4727d6d1771b0edcdbdf6cb7a0fefdee4f87a90c
Jack Reuter <jreuter@wesleyan.edu>
2020-01-16 00:14:12 -0500

    enhanced building graphics. completed shape validation functions. updated building list (#32)

--- client/buildingValidation.js
+++ client/buildingValidation.js
@@ -82,10 +82,31 @@ function clone(obj) {
         }
         return copy;
     }
-
     throw new Error("Unable to copy obj! Its type isn't supported.");
 }
 
+//infrastructure
+function boulevard(coords, move, board, startingPlayer) {
+  return ShapeUtils.checkShapeCane(coords)
+}
+function tunnel(coords, move, board, startingPlayer) {
+  if (coords.length != 1) {
+    return false
+  } else {
+    return true
+  }
+}
+function prison(coords, move, board, startingPlayer) {
+  return ShapeUtils.checkShapeDouble(coords)
+}
+function tramway(coords, move, board, startingPlayer) {
+  return ShapeUtils.checkShape3Line(coords)
+}
+function plaza(coords, move, board, startingPlayer) {
+  return ShapeUtils.checkShapeDiamond(coords)
+}
+
+//aquatic
 function harbor(coords, move, board, startingPlayer) {
   var row = coords[0]['row']
   var col = coords[0]['col']
@@ -96,7 +117,28 @@ function harbor(coords, move, board, startingPlayer) {
     return false
   }
 }
-
+function bridge(coords, move, board, startingPlayer) {
+  return ShapeUtils.checkShape3Line(coords)
+}
+function canal(coords, move, board, startingPlayer) {
+  return ShapeUtils.checkShapeCane(coords)
+}
+function lock(coords, move, board, startingPlayer) {
+  return ShapeUtils.checkShapeDiamond(coords)
+}
+function ferry(coords, move, board, startingPlayer) {
+  var row = coords[0]['row']
+  var col = coords[0]['col']
+  var tileAdjacency = ShapeUtils.tileAdjacencyCheck(row, col, move, board, startingPlayer)
+  if (
+    (ShapeUtils.friendly(row, col, board, startingPlayer) || (move['marker_placement']['row'] == row && move['marker_placement']['col'] == col))
+    && tileAdjacency.adjacentToWater
+  ) {
+    return true
+  } else {
+    return false
+  }
+}
 function lightHouse(coords, move, board, startingPlayer) {
   var row = coords[0]['row']
   var col = coords[0]['col']
@@ -111,84 +153,95 @@ function lightHouse(coords, move, board, startingPlayer) {
   }
 }
 
+//cultural
+function docks(coords, move, board, startingPlayer) {
+  return ShapeUtils.checkShapeCane(coords)
+}
+function embassy(coords, move, board, startingPlayer) {
+  return ShapeUtils.checkShapeV(coords)
+}
+function cathedral(coords, move, board, startingPlayer) {
+  return ShapeUtils.checkShapeV(coords)
+}
+function cityHall(coords, move, board, startingPlayer) {
+  return ShapeUtils.checkShapeTriangle(coords)
+}
+function marina(coords, move, board, startingPlayer) {
+  return ShapeUtils.checkShapeU(coords)
+}
+
+//commercial
+function tenement(coords, move, board, startingPlayer) {
+  return ShapeUtils.checkShapeDiamond(coords)
+}
+function bazaar(coords, move, board, startingPlayer) {
+  return ShapeUtils.checkShapeCane(coords)
+}
+function refinery(coords, move, board, startingPlayer) {
+  return ShapeUtils.checkShapeDouble(coords)
+}
+function casino(coords, move, board, startingPlayer) {
+  return ShapeUtils.checkShapeTriangle(coords)
+}
+function watchTower(coords, move, board, startingPlayer) {
+  if (coords.length != 1) {
+    return false
+  } else {
+    return true
+  }
+}
+
+//civic
 function taxHouse(coords, move, board, startingPlayer) {
   return ShapeUtils.checkShapeTriangle(coords)
 }
-
-function jail(coords, move, board, startingPlayer) {
-  return ShapeUtils.checkShapeDouble(coords)
+function cemetary(coords, move, board, startingPlayer) {
+  return ShapeUtils.checkShapeV(coords)
 }
-
-function quarry(coords, move, board, startingPlayer) {
-  return ShapeUtils.checkShapeDouble(coords)
+function shipyard(coords, move, board, startingPlayer) {
+  return ShapeUtils.checkShapeTriangle(coords)
 }
-
-function depot(coords, move, board, startingPlayer) {
-  return ShapeUtils.checkShapeDouble(coords)
-}
-
-function trolley(coords, move, board, startingPlayer) {
-  return ShapeUtils.checkShape3Line(coords)
-}
-
-function wall(coords, move, board, startingPlayer) {
-  return ShapeUtils.checkShape3Line(coords)
-}
-
-function bridge(coords, move, board, startingPlayer) {
-  return ShapeUtils.checkShape3Line(coords)
-}
-
 function sewers(coords, move, board, startingPlayer) {
   return ShapeUtils.checkShape3Line(coords)
 }
-
-function docks(coords, move, board, startingPlayer) {return true}
-function settlement(coords, move, board, startingPlayer) {return true}
-function bazaar(coords, move, board, startingPlayer) {return true}
-function bank(coords, move, board, startingPlayer) {return true}
-function embassy(coords, move, board, startingPlayer) {return true}
-function guardTower(coords, move, board, startingPlayer) {return true}
-function customsOffice(coords, move, board, startingPlayer) {return true}
-function casino(coords, move, board, startingPlayer) {return true}
-function graveyard(coords, move, board, startingPlayer) {return true}
-function workShop(coords, move, board, startingPlayer) {return true}
-function mill(coords, move, board, startingPlayer) {return true}
-function church(coords, move, board, startingPlayer) {return true}
-function boulevard(coords, move, board, startingPlayer) {return true}
-function aqueduct(coords, move, board, startingPlayer) {return true}
-function shipyard(coords, move, board, startingPlayer) {return true}
-function cityHall(coords, move, board, startingPlayer) {return true}
-function tunnel(coords, move, board, startingPlayer) {return true}
-function plaza(coords, move, board, startingPlayer) {return true}
+function monument(coords, move, board, startingPlayer) {
+  return ShapeUtils.checkShapeDouble(coords)
+}
 
 var buildingData = {
-  'Tax House': {'validation_function': taxHouse, 'length': 3},
-  'Docks': {'validation_function': docks, 'length': 4},
-  'Settlement': {'validation_function': settlement, 'length': 4},
-  'Bazaar': {'validation_function': bazaar, 'length': 4},
-  'Quarry': {'validation_function': quarry, 'length': 2},
-  'Bank': {'validation_function': bank, 'length': 3},
-  'Embassy': {'validation_function': embassy, 'length': 3},
-  'Guard Tower': {'validation_function': guardTower, 'length': 1},
-  'Customs Office': {'validation_function': customsOffice, 'length': 4},
-  'Casino': {'validation_function': casino, 'length': 3},
-  'Lighthouse': {'validation_function': lightHouse, 'length': 1},
-  'Graveyard': {'validation_function': graveyard, 'length': 3},
-  'Jail': {'validation_function': jail, 'length': 2},
-  'Workshop': {'validation_function': workShop, 'length': 4},
-  'Mill': {'validation_function': mill, 'length': 4},
-  'Wall': {'validation_function': wall, 'length': 3},
-  'Church': {'validation_function': church, 'length': 3},
+  //infrastructure
   'Boulevard': {'validation_function': boulevard, 'length': 4},
-  'Aqueduct': {'validation_function': aqueduct, 'length': 4},
-  'Harbor': {'validation_function': harbor, 'length': 1},
-  'Shipyard': {'validation_function': shipyard, 'length': 3},
-  'Trolley': {'validation_function': trolley, 'length': 3},
-  'City Hall': {'validation_function': cityHall, 'length': 3},
-  'Bridge': {'validation_function': bridge, 'length': 3},
   'Tunnel': {'validation_function': tunnel, 'length': 1},
+  'Prison': {'validation_function': prison, 'length': 2},
+  'Tramway': {'validation_function': tramway, 'length': 3},
+  'Plaza': {'validation_function': plaza, 'length': 4},
+
+  //aquatic
+  'Bridge': {'validation_function': bridge, 'length': 3},
+  'Harbor': {'validation_function': harbor, 'length': 1},
+  'Canal': {'validation_function': canal, 'length': 4},
+  'Lock': {'validation_function': lock, 'length': 4},
+  'Ferry': {'validation_function': ferry, 'length': 1},
+  'Lighthouse': {'validation_function': lightHouse, 'length': 1},
+
+  //cultural
+  'Docks': {'validation_function': docks, 'length': 4},
+  'Embassy': {'validation_function': embassy, 'length': 3},
+  'Cathedral': {'validation_function': cathedral, 'length': 3},
+  'City Hall': {'validation_function': cityHall, 'length': 3},
+  'Marina': {'validation_function': marina, 'length': 4},
+
+  //commericial
+  'Tenement': {'validation_function': tenement, 'length': 4},
+  'Bazaar': {'validation_function': bazaar, 'length': 4},
+  'Refinery': {'validation_function': refinery, 'length': 2},
+  'Casino': {'validation_function': casino, 'length': 3},
+  'Watchtower': {'validation_function': watchTower, 'length': 1},
+
+  //civic
+  'Tax House': {'validation_function': taxHouse, 'length': 3},
+  'Cemetary': {'validation_function': cemetary, 'length': 3},
+  'Shipyard': {'validation_function': shipyard, 'length': 3},
   'Sewers': {'validation_function': sewers, 'length': 3},
-  'Depot': {'validation_function': depot, 'length': 2},
-  'Plaza': {'validation_function': plaza, 'length': 4}
+  'Monument': {'validation_function': monument, 'length': 2}
 }
