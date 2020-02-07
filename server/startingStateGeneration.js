40e63ce2cdaa972dcd8f64ee07a4b6691d73384d
tdobrow <tomdobrow@gmail.com>
2020-02-07 08:37:56 -0600

    set percent water on board. also have set amount of each resource

--- server/startingStateGeneration.js
+++ server/startingStateGeneration.js
@@ -21,15 +21,15 @@
       {'name': 'Lighthouse', 'type': 'Aquatic', 'bm': 1, 'c': 3, 'l': 1, '?': 0, 'limit': 5, 'vp': 2},
 
       //cultural
-      {'name': 'Docks', 'type': 'Cultural', 'bm': 1, 'c': 1, 'l': 3, '?': 0, 'limit': 5, 'vp': '*'}, //
-      {'name': 'Embassy', 'type': 'Cultural', 'bm': 1, 'c': 2, 'l': 2, '?': 0, 'limit': 5, 'vp': '*'}, //
+      {'name': 'Docks', 'type': 'Cultural', 'bm': 1, 'c': 1, 'l': 3, '?': 0, 'limit': 5, 'vp': '*'},
+      {'name': 'Embassy', 'type': 'Cultural', 'bm': 1, 'c': 2, 'l': 2, '?': 0, 'limit': 5, 'vp': '*'},
       {'name': 'Cathedral', 'type': 'Cultural', 'bm': 2, 'c': 3, 'l': 0, '?': 0, 'limit': 5, 'vp': 5},
       {'name': 'City Hall', 'type': 'Cultural', 'bm': 1, 'c': 1, 'l': 1, '?': 0, 'limit': 5, 'vp': 3},
       {'name': 'Marina', 'type': 'Cultural', 'bm': 2, 'c': 3, 'l': 0, '?': 0, 'limit': 5, 'vp': 6},
 
       //commercial
       {'name': 'Tenement', 'type': 'Commercial', 'bm': 0, 'c': 0, 'l': 0, '?': 4, 'limit': 4, 'vp': 3},
-      {'name': 'Bazaar', 'type': 'Commercial', 'bm': 0, 'c': 0, 'l': 0, '?': 6, 'limit': 5, 'vp': 3}, //
+      {'name': 'Bazaar', 'type': 'Commercial', 'bm': 0, 'c': 0, 'l': 0, '?': 6, 'limit': 5, 'vp': 3},
       {'name': 'Refinery', 'type': 'Commercial', 'bm': 0, 'c': 1, 'l': 3, '?': 0, 'limit': 5, 'vp': 2}, 
       {'name': 'Casino', 'type': 'Commercial', 'bm': 1, 'c': 0, 'l': 3, '?': 0, 'limit': 5, 'vp': 0},
       {'name': 'Watchtower', 'type': 'Commercial', 'bm': 0, 'c': 2, 'l': 3, '?': 0, 'limit': 5, 'vp': 3},
@@ -43,9 +43,9 @@
     ]
 
     return [
-      {'name': 'Bazaar', 'type': 'Commercial', 'bm': 0, 'c': 0, 'l': 0, '?': 6, 'limit': 5, 'vp': 3}, //
+      {'name': 'Bazaar', 'type': 'Commercial', 'bm': 0, 'c': 0, 'l': 0, '?': 6, 'limit': 5, 'vp': 3},
       {'name': 'Lighthouse', 'type': 'Aquatic', 'bm': 1, 'c': 3, 'l': 1, '?': 0, 'limit': 5, 'vp': 2},
-      {'name': 'Embassy', 'type': 'Cultural', 'bm': 1, 'c': 2, 'l': 2, '?': 0, 'limit': 5, 'vp': '*'}, //
+      {'name': 'Embassy', 'type': 'Cultural', 'bm': 1, 'c': 2, 'l': 2, '?': 0, 'limit': 5, 'vp': '*'},
       {'name': 'Watchtower', 'type': 'Commercial', 'bm': 0, 'c': 2, 'l': 3, '?': 0, 'limit': 5, 'vp': 3},
       {'name': 'Lock', 'type': 'Aquatic', 'bm': 2, 'c': 2, 'l': 0, '?': 0, 'limit': 5, 'vp': 2},
     ]
@@ -58,17 +58,41 @@
     // return results
   }
 
-  module.exports.generateBoard = function(num_rows, num_cols) {
+  module.exports.generateBoard = function(num_rows, num_cols, percent_water) {
     board = []
     p1_resources = {'bm':0, 'l':0, 'c':0}
     p2_resources = {'bm':0, 'l':0, 'c':0}
+    tiles_left = []
+    var i=0
+    num_water = parseInt(96* (percent_water/100))
+    num_bm = parseInt((96 - num_water)/3)
+    num_l = parseInt((96 - num_water - num_bm)/2)
+    num_c = parseInt(96 - num_water - num_bm - num_l)
+    while (i < num_bm) {
+      tiles_left.push('bm')
+      i++
+    }
+    while (i < num_bm + num_l) {
+      tiles_left.push('l')
+      i++
+    }
+    while (i < num_bm + num_l + num_c) {
+      tiles_left.push('c')
+      i++
+    }
+    while (i < num_bm + num_l + num_c + num_water) {
+      tiles_left.push('w')
+      i++
+    }
     for (row=0; row<num_rows; row+=1) {
       new_row = []
-      //num_columns = (row%2 == 0) ? 8 : 9
+
       num_columns = num_cols
       for (col=0; col<num_columns; col+=1) {
         const markerType = getMarker(row, col, num_rows, num_cols)
-        type = getTileType(markerType)
+        results = getTileType(markerType, tiles_left)
+        type = results[0]
+        tiles_left = results[1]
         if (markerType == "player_one") {
           p1_resources[type] += 1 
         } else if (markerType == "player_two") {
@@ -87,18 +111,18 @@
 
   // Private helper function
 
-  function getTileType(markerType) {
-    x = Math.floor(Math.random() * 33)
+  function getTileType(markerType, tiles_left) {
+    x = Math.floor(Math.random() * tiles_left.length)
+    var returnType = ""
     if (markerType != 'empty') {
-      if (x < 11) { return 'bm' }
-      if (x < 22) { return 'l' }
-      return 'c'
+      if (x < tiles_left.length/3) { returnType = 'bm' }
+      else if (x < (2*tiles_left.length)/3) { returnType = 'l' }
+      else { returnType = 'c' }
     } else {
-      if  (x < 9) { return 'bm' }
-      if (x < 18) { return 'l' }
-      if (x < 27) { return 'c' }
-      return 'w'
+      returnType = tiles_left[x]
     } 
+    tiles_left.splice(x, 1)
+    return [returnType, tiles_left]
   }
 
   function getMarker(row, col, num_rows, num_cols) {
